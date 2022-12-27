a3840d63e6bc8fc662c5d165ad431cca44db9b1c
ellieleung12 <113403153+ellieleung12@users.noreply.github.com>
2022-12-27 22:35:57 +0800

    Update p01.py

--- exam/p/p01.py
+++ exam/p/p01.py
@@ -1,6 +1,10 @@
 def p01(*nums):
     minnum=None
     # ↓程式區域↓
-
+    minnum = nums[0]
+    for i in range(1,len(nums)-1):
+        if nums[i]< minnum:
+            minnum = nums[i]
+            nums.append(nums)
     # ↑程式區域↑
     return minnum
